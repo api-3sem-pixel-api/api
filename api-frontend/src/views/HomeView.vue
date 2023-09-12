0db8b1089a2902ba505ceaae6e4d5e681bd0be39
Matheus Fiebig <macrubig@gmail.com>
2023-09-12 19:16:31 -0300

    feature: main content

--- api-frontend/src/views/HomeView.vue
+++ api-frontend/src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
-  <Sidebar></Sidebar>
-  <div class="content"></div>
+  <div class="page">
+    <Sidebar></Sidebar>
+    <div class="content">
+      <div class="card">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,8 +23,23 @@ export default class HomeView extends Vue {
 </script>
 
 <style scoped>
+.page {
+  width: 100%;
+  background-color: rgba(235, 235, 235, 0.925);
+}
+
 .content {
-  margin: 75px 0 0 275px;
-  width: calc(100vw - 303px);
+  height: inherit;
+  margin: 75px 0 0 300px;
+  height: 100vh;
+  width: calc(100vw - 350px);
+}
+
+.card {
+  background-color: white;
+  box-shadow: 0px 0px 5px #dddddd;
+  height: 95%;
+  border-radius: 5px;
+  width: 100%;
 }
 </style>
