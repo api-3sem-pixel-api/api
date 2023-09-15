3dd1f99f801b9c7aa9f417d15786debcbc70896c
lulu20000 <luciano.nascimentopamplona@gmail.com>
2023-09-14 22:29:05 -0300

    Feature: atualização da tela e criação do botão

--- api-frontend/src/views/LancamentoHoras/LancamentoHorasView.vue
+++ api-frontend/src/views/LancamentoHoras/LancamentoHorasView.vue
@@ -17,15 +17,27 @@
       <p class="italic">Projeto</p>
       <input type="text" class="form-control">      
     </div>
+
+
+
     <div class="col">
       <p class="italic">Inicio</p>
       <input id="party" type="datetime-local" name="partydate" />
     </div>
+
+    
+
+    
     <div class="col">
       <p class="italic">Fim</p>
-      <input id="party" type="datetime-local" name="partydate" />   
+      <input class="button-s" id="party" type="datetime-local"  name="partydate"  /> 
     </div>
-    <TabelaHoras></TabelaHoras>
+    <div class="col" style="margin-top:30px;"><button type="button" class="btn btn-outline-primary">Lançar</button></div>
+
+
+
+
+    <TabelaHoras style="margin-top: 100px;"></TabelaHoras>
     
 
   </div>
@@ -57,5 +69,9 @@ export default class LancamentoHorasView extends Vue {
 .form-select{
   width: 0.75;
 }
+.button-s{
+  boder:1px solid #000;
+}
+
 
 </style>
