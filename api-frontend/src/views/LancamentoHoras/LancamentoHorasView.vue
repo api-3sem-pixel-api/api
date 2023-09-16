9a20793b37b7e6ff0a70cef17673214f3410908c
luKas <lucas.lucas000@gmail.com>
2023-09-15 22:19:04 -0300

    feature: laçamento de horas

--- api-frontend/src/views/LancamentoHoras/LancamentoHorasView.vue
+++ api-frontend/src/views/LancamentoHoras/LancamentoHorasView.vue
@@ -3,26 +3,26 @@
   <div class="row ">
     <div class="col">
       <p class="italic">Modalidade</p>
-      <select class="form-select" aria-label=".form-select-lg example"></select>
+      <select class="form-select" aria-label=".form-select-lg example" v-model="lancamento.modalidade"></select>
     </div>
     <div class="col">
       <p class="italic">CR</p>
-      <select class="form-select" aria-label=".form-select-lg example"></select>
+      <select class="form-select" aria-label=".form-select-lg example"  v-model = "lancamento.cr"></select>
     </div>
     <div class="col">
       <p class="italic">Cliente</p>
-      <select class="form-select" aria-label=".form-select-lg example"></select>
+      <select class="form-select" aria-label=".form-select-lg example" v-model = "lancamento.cliente"></select>
     </div>
     <div class="col">
       <p class="italic">Projeto</p>
-      <input type="text" class="form-control">      
+      <input type="text" class="form-control" v-model = "lancamento.projeto">       
     </div>
 
 
 
     <div class="col">
       <p class="italic">Inicio</p>
-      <input id="party" type="datetime-local" name="partydate" />
+      <input id="party" type="datetime-local" name="partydate" v-model ="lancamento.inicio" />
     </div>
 
     
@@ -30,9 +30,9 @@
     
     <div class="col">
       <p class="italic">Fim</p>
-      <input class="button-s" id="party" type="datetime-local"  name="partydate"  /> 
+      <input class="button-s" id="party" type="datetime-local"  name="partydate" v-model = "lancamento.fim" /> 
     </div>
-    <div class="col" style="margin-top:30px;"><button type="button" class="btn btn-outline-primary">Lançar</button></div>
+    <div class="col" style="margin-top:30px;"><button v-on:click ="lancar" type="button" class="btn btn-outline-primary">Lançar</button></div>
 
 
 
@@ -48,16 +48,34 @@
 <script lang="ts">
 import TabelaHoras from './TabelaHoras/TabelaHoras.vue';
 import { Options, Vue } from 'vue-class-component';
+import http from "@/services/http";
 
 
 @Options({
-
   components: {
     TabelaHoras
   }
-  
 })
 export default class LancamentoHorasView extends Vue {
+  lancamento:any = {
+      id: '',
+      cr: '',
+      cliente: '',
+      projeto:'',
+      inicio:'',
+      fim:'',
+      modalidade:'',
+      solicitante:'',
+      justificativa:'',
+      status:'',
+  }
+
+ async lancar(){
+
+  console.log(this.lancamento)
+    const {data} = await http.post("/lancamentoHoras", this.lancamento);
+    
+  }
 };
 </script>
 
