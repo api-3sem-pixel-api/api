fc224de35a9de7078efc8c9aa68fc518d6ff800a
zZzidanillo <https://zZzidanillo@github.com>
2023-09-29 21:55:06 -0300

    feat: crud usuario

--- api-backend/src/main/java/fatec/api/pixel/horaextra/service/UsuarioService.java
+++ api-backend/src/main/java/fatec/api/pixel/horaextra/service/UsuarioService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fatec.api.pixel.horaextra.dto.DadosCadastroUsuario;
+import fatec.api.pixel.horaextra.model.TipoUsuario;
 import fatec.api.pixel.horaextra.model.Usuario;
 import fatec.api.pixel.horaextra.repository.UsuarioRepository;
 
@@ -16,15 +17,24 @@ public class UsuarioService {
 	@Autowired
 	AutenticacaoUsuarioService autenticacaoUsuarioService;
 	
-	public void excluirUsuario(Usuario usuario) {
-		usuario.setAtivo(false);
+	public void atualizarUsuario(DadosCadastroUsuario dados, Long id) {
+		var atualizacaoUsuario = repository.getReferenceById(id);
+		atualizacaoUsuario.setTipoUsuario(new TipoUsuario(dados.idTipoUsuario()));
+		atualizacaoUsuario.setCpf(dados.cpf());
+		atualizacaoUsuario.setNome(dados.nome());
+		atualizacaoUsuario.setTelefone(dados.telefone());
+		atualizacaoUsuario.setEmail(dados.email());
 	}
-	
+		
 	public Usuario inserirUsuario(DadosCadastroUsuario dados) {
 		var usuario = new Usuario(dados);
 		var autenticacaoUsuario = autenticacaoUsuarioService.insert(dados.cpf());
 		usuario.setAutenticacaoUsuario(autenticacaoUsuario);
 		repository.save(usuario);
 		return usuario;
+	}
+	
+	public void excluirUsuario(Usuario usuario) {
+		usuario.setAtivo(false);
 	}
 }
