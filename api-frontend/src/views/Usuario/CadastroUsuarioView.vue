d0341bb0fff0a3e30b61e346f7755974c9e3880e
4l1son <alisson.pereira5@fatec.sp.gov.br>
2023-10-10 10:48:31 -0300

    feat:finalizando get e ajuste no update

--- api-frontend/src/views/Usuario/CadastroUsuarioView.vue
+++ api-frontend/src/views/Usuario/CadastroUsuarioView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-content-end mb-3">
-    <button class="btn btn-outline-primary" @click="newUser()"> Cadastrar Usuario </button>
+    <button class="btn btn-outline-primary" @click="newUser()"> Cadastrar Usuário </button>
   </div>
   <div class="row">
     <table class="table table-responsive no-wrap-table">
@@ -8,49 +8,59 @@
         <tr>
           <th scope="col" class="text-left">Nome</th>
           <th scope="col" class="text-left">Email</th>
-          <th scope="col" class="text-left ">Telefone</th>
-          <th scope="col" class="text-left ">CPF</th> 
+          <th scope="col" class="text-left">Telefone</th>
+          <th scope="col" class="text-left">CPF</th>
           <th scope="col" class="text-center">Função</th>
           <th scope="col" class="text-center">Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(usuario, index) in usuarios" :key="index">
-  <td>{{ usuario['nome'] }}</td>
-  <td>{{ usuario['email'] }}</td>
-  <td>{{ usuario['telefone'] }}</td>
-  <td>{{ usuario['cpf'] }}</td>
-  <td>{{ usuario['funcao'] }}</td>
-  <td class="text-center">
-    <button class="btn btn-link" @click="updateUser()">
-      <i class="fa fa-pencil" aria-hidden="true"></i>
-    </button>
-    <button class="btn btn-link" @click="excluedUser(index)">
-      <i class="fa fa-trash" aria-hidden="true"></i>
-    </button>
-  </td>
-</tr>
+          <td>{{ usuario['nome'] }}</td>
+          <td>{{ usuario['email'] }}</td>
+          <td>{{ usuario['telefone'] }}</td>
+          <td>{{ usuario['cpf'] }}</td>
+          <td>{{ enumUser[usuario['funcao']] }}</td>
+          <td class="text-center">
+            <button class="btn btn-link" @click="updateUser(usuario.id)">
+              <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button class="btn btn-link" @click="excludedUser(index)">
+              <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
 
-  <ModalUsuarioView @update-table="loadAllUser()"></ModalUsuarioView>
+  <!-- Modal de Atualização de Usuário -->
+  <ModalUsuarioView
+    :user-id="editUserId"
+    @update-user-details="updateUserDetails"
+    @close-modal="closeUpdateModal"
+  ></ModalUsuarioView>
 </template>
+
 <script lang="ts">
 import http from '@/services/http';
 import { PropType, defineComponent } from 'vue';
 import ModalUsuarioView from '@/views/Usuario/ModalCadastroUsuario/ModalCadastroUsuarioView.vue';
 import ModalCadastroUpdateUserView from './ModalCadastroUsuario/ModalCadastroUpdateUserView.vue';
+import { enumUser } from '@/views/Usuario/enumUser';
+
 export default defineComponent({
   name: "ControleUsuarioView",
   components: {
     ModalUsuarioView,
     ModalCadastroUpdateUserView
-
   },
   data() {
     return {
-      usuarios: [] as Array<any>     }
+      usuarios: [] as Array<any>,
+      enumUser: enumUser,
+      editUserId: null as number | null 
+    };
   },
   created() {
     this.loadAllUser();
@@ -63,25 +73,49 @@ export default defineComponent({
     loadAllUser() {
       http.get('/usuario')
         .then(x => this.usuarios = x.data)
-        .catch(err => alert('Algo deu errado tente novamente mais tarde.'));
+        .catch(err => alert('Algo deu errado, tente novamente mais tarde.'));
     },
-    updateUser() {
-  var modal = document.getElementById("update-user-modal");
-  if (modal) {
-    modal.style.display = "block";
-  } else {
-    console.error("Element with id 'update-user-modal' not found in the DOM.");
-  }
-},
-    async excluedUser(index: number) {
+    updateUser(userId: number) {
+      this.editUserId = userId;
+      var modal = document.getElementById("update-user-modal");
+      if (modal) {
+        modal.style.display = "block";
+      } else {
+        console.error("Elemento não encontrado no DOM.");
+      }
+    },
+
+    async updateUserDetails(updatedUser: any) {
+      try {
+        await http.put(`/usuario/${this.editUserId}`, updatedUser);
+
+        // Atualize os dados do usuário no array this.usuarios
+        const userIndex = this.usuarios.findIndex(u => u.id === this.editUserId);
+        if (userIndex !== -1) {
+          this.usuarios[userIndex] = updatedUser;
+        }
+
+        this.closeUpdateModal(); 
+      } catch (error) {
+        alert('Erro ao atualizar o usuário. Tente novamente mais tarde.');
+      }
+    },
+    closeUpdateModal() {
+      this.editUserId = null; 
+      var modal = document.getElementById("update-user-modal");
+      if (modal) {
+        modal.style.display = "none";
+      }
+    },
+    async excludedUser(index: number) {
       const usuario = this.usuarios[index];
       try {
         await http.delete(`/usuario/${usuario.id}`);
-        this.usuarios.splice(index, 1); 
+        this.usuarios.splice(index, 1);
       } catch (error) {
         alert('Algo deu errado, tente novamente mais tarde.');
       }
     }
   }
-})
+});
 </script>
