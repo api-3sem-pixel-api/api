06dda7b919bda65cc9c58973db70a9be0015dccd
4l1son <alisson.pereira5@fatec.sp.gov.br>
2023-10-12 11:30:02 -0300

    feat: combox

--- api-frontend/src/views/Usuario/ModalCadastroUsuario/ModalCadastroUsuarioView.vue
+++ api-frontend/src/views/Usuario/ModalCadastroUsuario/ModalCadastroUsuarioView.vue
@@ -1,72 +1,69 @@
 <template>
-    <div id="cadastro-user-modal" class="r-modal">
-      <div class="r-modal-content">
-        <div class="modal-header d-flex align-items-baseline">
-          <h4>Cadastro de Usuario</h4>
-          <span class="close" @click="close">&times;</span>
+  <div id="cadastro-user-modal" class="r-modal">
+    <div class="r-modal-content">
+      <div class="modal-header d-flex align-items-baseline">
+        <h4>Cadastro de Usuário</h4>
+        <span class="close" @click="close">&times;</span>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <label for="nome">Nome</label>
+              <input type="text" class="form-control" id="nome" v-model="nome" />
+            </div>
+          </div>
         </div>
-        <div class="modal-body">
-          <div class="row">
-            <div class="col-12">
-              <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" v-model="nome" />
-              </div>
+
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <label for="telefone">Telefone</label>
+              <input type="text" class="form-control" id="telefone" v-model="telefone" />
             </div>
           </div>
-  
-          <div class="row">
-            <div class="col-12">
-              <div class="form-group">
-                <label for="telefone">Telefone</label>
-                <input type="text" class="form-control" id="telefone" v-model="telefone" />
-              </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="text" class="form-control" id="email" v-model="email" />
             </div>
           </div>
-          <div class="row">
-            <div class="col-12">
-              <div class="form-group">
-                <label for="email">Email</label>
-                <input type="text" class="form-control" id="email" v-model="email" />
-              </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <label for="cpf">CPF</label>
+              <input type="text" class="form-control" id="cpf" v-model="cpf" />
             </div>
           </div>
-          <div class="row">
-            <div class="col-12">
-              <div class="form-group">
-                <label for="cpf">CPF</label>
-                <input type="text" class="form-control" id="cpf" v-model="cpf" />
-              </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <label for="funcao">Função</label>
+              <select class="form-select" id="funcao" v-model="funcao">
+                <option value="Colaborador">Colaborador</option>
+                <option value="Administrador">Administrador</option>
+                <option value="Gestor">Gestor</option>
+              </select>
             </div>
           </div>
-  
-          <div class="row">
-            <div class="col-12">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="colaborador" value="Colaborador" v-model="funcao" />
-                <label class="form-check-label" for="colaborador">Colaborador</label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="administrador" value="Administrador" v-model="funcao" />
-                <label class="form-check-label" for="administrador">Administrador</label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gestor" value="Gestor" v-model="funcao" />
-                <label class="form-check-label" for="gestor">Gestor</label>
-              </div>
-              <div class="row mt-4">
-                <div class="col">
-                  <button type="button" @click="save()" class="btn btn-success">Salvar</button>
-                  <button type="button" @click="close()" class="btn btn-link r-ml-2">Cancelar</button>
-                </div>
-              </div>
-            </div>
+        </div>
+
+        <div class="row mt-4">
+          <div class="col">
+            <button type="button" @click="save" class="btn btn-success">Salvar</button>
+            <button type="button" @click="close" class="btn btn-link r-ml-2">Cancelar</button>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
   <script lang="ts">
   import http from '@/services/http';
   import { defineComponent } from 'vue';
