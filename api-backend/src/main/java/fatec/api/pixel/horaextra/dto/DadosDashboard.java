02bc43b0c52efb88018ac306eeb0f440296bc58c
4l1son <alisson.pereira5@fatec.sp.gov.br>
2023-10-30 21:55:13 -0300

    feat<cliente>:implementacao da inativacao do cliente

--- api-backend/src/main/java/fatec/api/pixel/horaextra/dto/DadosDashboard.java
+++
@@ -1,7 +0,0 @@
-package fatec.api.pixel.horaextra.dto;
-
-import java.util.Date;
-
-public record DadosDashboard(Long idCliente, Long idCr, Date dataInicio, Date dataFim) {
-
-}
--- api-frontend/src/views/Cliente/VizuCliente.vue
+++ api-frontend/src/views/Cliente/VizuCliente.vue
@@ -1,58 +1,44 @@
 <template>
-  <Cliente @update-table="loadAllcliente"></Cliente>
-  <div class="row">
-    <table class="table table-responsive no-wrap-table">
-      <thead>
-        <tr>
-          <th scope="col" class="text-left">Razão Social</th>
-          <th scope="col" class="text-left">Cnpj</th>
-        
-          <th scope="col" class="text-center">Status</th>
-          <th scope="col" class="text-center">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(client, index) in clientes" :key="index">
-      
-         
-          <td>{{ client['razaoSocialCliente'] }}</td>
-          <td>{{ client['cnpjCliente'] }}</td>
-          <td class="text-center d-flex" style="justify-content: center;"  > <div 
-          class="pill approved text-center text-wrap" 
-          :class="{
-            approved: client['ativo'] == true,
-        }" > 
-            Ativo 
-        </div></td>
-          <td class="text-center">
-            <button class="btn btn-link"><i class="fa fa-pencil" aria-hidden="true"></i></button>
-
-        <button class="btn btn-link" @click="inativarCliente(client['id'])"><i class="fa fa-trash"  aria-hidden="true" ></i></button>
-
-        
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <Cliente @update-table="loadAllCliente"></Cliente>
+    <div class="row">
+      <table class="table table-responsive no-wrap-table">
+        <thead>
+          <tr>
+            <th scope="col" class="text-left">Razão Social</th>
+            <th scope="col" class="text-left">Cnpj</th>
+            <th scope="col" class="text-center">Status</th>
+            <th scope="col" class="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="cliente in clientes" :key="cliente.id">
+            <td>{{ cliente.razaoSocialCliente }}</td>
+            <td>{{ cliente.cnpjCliente }}</td>
+            <td class="d-flex justify-content-center">
+              <div
+                class="pill text-center text-wrap"
+                :class="{ approved: cliente.ativo, inativo: !cliente.ativo }"
+              >
+                {{ cliente.ativo ? 'Ativo' : 'Inativo' }}
+              </div>
+            </td>
+            <td class="text-center">
+              <button class="btn btn-link"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+              <button class="btn btn-link" @click="inativarCliente(cliente['idCliente'])"><i class="fa fa-trash" aria-hidden="true"></i></button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-  
-
-
 </template>
-
-
 
 <script lang="ts">
 import http from '@/services/http';
 import { PropType, defineComponent } from 'vue';
 import Cliente from '@/views/Cliente/Cliente.vue';
 
-interface ClienteData {
-  id: number;
-  razaoSocialCliente: string;
-  cnpjCliente: string;
-  ativo: boolean;
-}
 
 export default defineComponent({
   name: "VizuCliente",
@@ -62,16 +48,16 @@ export default defineComponent({
 
   data() {
     return {
-      clientes: [] as ClienteData[]
+      clientes: [] as  Array<any>
     };
   },
 
   created() {
-    this.loadAllcliente();
+    this.loadAllCliente();
   },
 
   methods: {
-    async loadAllcliente() {
+    async loadAllCliente() {
       try {
         const response = await http.get('/cliente');
         this.clientes = response.data;
@@ -81,26 +67,26 @@ export default defineComponent({
       }
     },
 
-    async inativarCliente(clienteId: number) {
-  const cliente = this.clientes.find((cliente) => cliente.id === clienteId);
-  console.log(cliente);
-  if (cliente) {
-    if (cliente.ativo) {
-      try {
-        await http.delete(`/cliente/${clienteId}`);
-        cliente.ativo = false;
-        alert('Cliente inativado com sucesso');
-      } catch (error) {
-        alert('Erro ao inativar o cliente. Tente novamente mais tarde.');
-      }
-    } else {
-      alert('O cliente já está inativo');
-    }
-  } else {
-    alert('Cliente não encontrado');
-  }
-}
+    async inativarCliente(idCliente:number) {
+      console.log(idCliente)
+      const cliente = this.clientes.find((c) => c.idCliente === idCliente);
 
+      if (cliente) {
+        if (cliente.ativo) {
+          try {
+            await http.delete(`/cliente/${idCliente}`);
+            cliente.ativo = false;
+            alert('Cliente inativado com sucesso');
+          } catch (error) {
+            alert('Erro ao inativar o cliente. Tente novamente mais tarde.');
+          }
+        } else {
+          alert('O cliente já está inativo');
+        }
+      } else {
+        alert('Cliente não encontrado');
+      }
+    }
   },
 });
 </script>
@@ -116,12 +102,7 @@ export default defineComponent({
   background-color: #26fc29;
 }
 
-.waiting {
-  background-color: gainsboro;
-}
-
-.reproved,
-.canceled {
-  background-color: red;
+.inativo {
+  background-color: red; /* Defina a cor desejada para clientes inativos */
 }
 </style>
