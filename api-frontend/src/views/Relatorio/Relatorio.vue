c1baf29b8dce5d5ccd83861b5f940e6bab7295fc
lulu20000 <luciano.nascimentopamplona@gmail.com>
2023-11-01 21:25:49 -0300

    feature: Atualização

--- api-frontend/src/views/Relatorio/Relatorio.vue
+++ api-frontend/src/views/Relatorio/Relatorio.vue
@@ -1,14 +1,20 @@
 <template>
+    <div class="row mt-4 mb-4">
+      <div class="col-12">
+      <h3>RELATÓRIO </h3>
+      <hr>
+      </div>
+    </div>
     <div class="row relatorio">
         <div class="col-3">
             <p class="bold">Data Inicio</p>
-            <input class="party" id="party" type="date" name="partydate" v-model="relatorio.datainicio" />
+            <input class="form-control" id="party" type="date" name="partydate" v-model="relatorio.datainicio" />
        </div> 
        <div class="col-3">
         <p class="bold">Data Fim</p>
-        <input class="party" id="party" type="date" name="partydate" v-model="relatorio.datafim" />
+        <input class="form-control" id="party" type="date" name="partydate" v-model="relatorio.datafim" />
        </div> 
-       <div class="col-1" style="margin-top:20px;"><button @click="gerar()" type="button"
+       <div class="col-4" style="margin-top:40px;"><button @click="gerar()" type="button"
         class="btn btn-outline-primary"> Gerar relatório </button></div>
         
     </div>
