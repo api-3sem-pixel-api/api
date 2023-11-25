70ae70e4b9dbdee7163d9f28842c094015a21fa6
luKas <lucas.lucas000@gmail.com>
2023-11-24 21:24:49 -0300

    feat:implementacao do modal do cr

--- api-frontend/src/views/Cr/ControleCrView.vue
+++ api-frontend/src/views/Cr/ControleCrView.vue
@@ -1,52 +1,102 @@
 <template>
-  <div class="row mt-4">
-    <div class="col-12">
-      <h3>CONTROLE | CR</h3>
-      <hr>
-    </div>
-  </div>
+  <div>
 
-  <div class="d-flex mt-3 justify-content-end mb-3">
-    <button class="btn btn-outline-primary" @click="newCr()"> Cadastrar CR </button>
-  </div>
-  <div class="row mt-4">
-    <table class="table table-responsive no-wrap-table">
-      <thead>
-        <tr>
-          <th scope="col" class="text-left">Cod</th>
-          <th scope="col" class="text-left">Sigla</th>
-          <th scope="col" class="text-left ">Nome</th>
-          <th scope="col" class="text-center">Status</th>
-          <th scope="col" class="text-center">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="cr in crs" :key="cr.id">
-          <td>{{ cr['codigo'] }}</td>
-          <td>{{ cr['sigla'] }}</td>
-          <td>{{ cr['nome'] }}</td>
-          <td style="width: 100px">  
-            <div 
-              class="pill text-center text-wrap" 
-              :class="{
-                approved: cr['ativo'] === true,
-                inativo: cr['ativo'] === false,
-              }"> 
-                {{ cr.ativo ? 'Ativo' : 'Inativo' }}
+    <div id="update-cr-modall" class="r-modal">
+      <div class="r-modal-content">
+        <div class="modal-header d-flex align-items-baseline">
+          <h4>Edição de Cliente</h4>
+          <span class="close" @click="close">&times;</span>
+        </div>
+       
+        <div class="row">
+          <div class="col-12" style="margin-top: 20px;">
+            <div class="form-group">
+              <label for="sigla">NOME</label>
+              <input type="text" class="form-control" id="sigla" v-model="siglaNome">
             </div>
-          </td>
-          <td class="text-center">
-            <button class="btn btn-link"><i class="fa fa-pencil" aria-hidden="true"></i></button>
-            <button class="btn btn-link" @click="editUserCr(cr['id'])"><i class="fas fa-id-card" aria-hidden="true"></i></button>
-            <button class="btn btn-link" @click="inativarCr(cr['id'])"><i class="fa fa-trash" aria-hidden="true"></i></button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12" style="margin-top: 20px;">
+            <div class="form-group">
+              <label for="sigla">SIGLA</label>
+              <input type="text" class="form-control" id="sigla" v-model="sigla">
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12" style="margin-top: 20px;">
+            <div class="form-group">
+              <label for="sigla">CODIGOCR</label>
+              <input type="text" class="form-control" id="sigla" v-model="siglaCodigoCr">
+            </div>
+          </div>
+        </div>
+
+        <div class="row mt-4">
+          <div class="col">
+            <button type="button" @click="save()" class="btn btn-success">Salvar</button>
+            <button type="button" @click="close()" class="btn btn-link r-ml-2">Cancelar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-12">
+        <h3>CONTROLE | CR</h3>
+        <hr>
+      </div>
+    </div>
+
+    <div class="d-flex mt-3 justify-content-end mb-3">
+      <button class="btn btn-outline-primary" @click="newCr()"> Cadastrar CR </button>
+    </div>
+
+    <div class="row mt-4">
+      <table class="table table-responsive no-wrap-table">
+        <thead>
+          <tr>
+            <th scope="col" class="text-left">Cod</th>
+            <th scope="col" class="text-left">Sigla</th>
+            <th scope="col" class="text-left">Nome</th>
+            <th scope="col" class="text-center">Status</th>
+            <th scope="col" class="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="cr in crs" :key="cr.idCr">
+            <td>{{ cr.codigo }}</td>
+            <td>{{ cr.sigla }}</td>
+            <td>{{ cr.nome }}</td>
+            <td style="width: 100px">
+              <div
+                class="pill text-center text-wrap"
+                :class="{
+                  approved: cr.ativo === true,
+                  inativo: cr.ativo === false,
+                }"
+              >
+                {{ cr.ativo ? 'Ativo' : 'Inativo' }}
+              </div>
+            </td>
+            <td class="text-center">
+              <button class="btn btn-link"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+              <button class="btn btn-link" @click="editUserCr"><i class="fas fa-id-card" aria-hidden="true"></i></button>
+              <button class="btn btn-link" @click="inativarCr(cr.idCr)"><i class="fa fa-trash" aria-hidden="true"></i></button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <ModalCrUsuario :id-cr="idCr"></ModalCrUsuario>
+    <ModalCadastroCrView @update-table="loadAllCr"></ModalCadastroCrView>
   </div>
-  <ModalCrUsuario :id-cr="idCr"></ModalCrUsuario>
-  <ModalCadastroCrView @update-table="loadAllCr"></ModalCadastroCrView>
 </template>
+
 <script lang="ts">
 import http from '@/services/http';
 import { PropType, defineComponent, ref } from 'vue';
@@ -74,9 +124,9 @@ export default defineComponent({
       modal.style.display = "block";
     },
     editUserCr(id: number) {
-      this.idCr = id;
-      var modal = document.getElementById("cr-usuario-modal")!;
+      var modal = document.getElementById("update-cr-modall")!;
       modal.style.display = "block";
+      console.log(modal)
     },
     loadAllCr() {
       http.get('/cr')
@@ -112,6 +162,7 @@ export default defineComponent({
   }
 })
 </script>
+
 
 <style scoped>
 .pill {
